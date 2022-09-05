c12d60edbebec3dda21a3704fa9d7bfbe3a0f6c0
Bartek Fryz <bartlomiej.fryz@swmansion.com>
2022-09-05 14:55:04 +0200

    Adds local fork of the turbo library

--- android/app/src/main/java/com/hotwirerndemo/RNSession.kt
+++ android/app/src/main/java/com/hotwirerndemo/RNSession.kt
@@ -1,7 +1,29 @@
 package com.hotwirerndemo
 import android.content.Context
+import android.util.Log
 import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.bridge.ReactContext
+import dev.hotwire.turbo.session.TurboSession
+import dev.hotwire.turbo.views.TurboWebView
 
 class RNSession(context: Context) : FrameLayout(context) {
+
+    private lateinit var session: TurboSession private set
+    private val reactContext = context as ReactContext
+
+    init {
+        createNewSession()
+    }
+
+    private fun createNewSession() {
+        val activity = reactContext.currentActivity as AppCompatActivity
+        session = TurboSession("testSessionName", activity, onCreateWebView(activity))
+        Log.d("RNSession", "created session")
+    }
+
+    private fun onCreateWebView(context: Context): TurboWebView {
+        return TurboWebView(context, null)
+    }
 
 }
