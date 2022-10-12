94cc513c087eace23262af38db2dcb811e415487
Bartek Fryz <bartlomiej.fryz@swmansion.com>
2022-10-12 09:33:33 +0200

    Adds Android native to JS messaging

--- android/app/src/main/java/com/hotwirerndemo/RNSession.kt
+++ android/app/src/main/java/com/hotwirerndemo/RNSession.kt
@@ -1,14 +1,17 @@
 package com.hotwirerndemo
 import android.content.Context
 import android.util.Log
+import android.webkit.JavascriptInterface
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.uimanager.events.RCTEventEmitter
 import dev.hotwire.turbo.session.TurboSession
-import dev.hotwire.turbo.views.TurboView
 import dev.hotwire.turbo.views.TurboWebView
-import dev.hotwire.turbo.visit.TurboVisit
 import java.util.*
+import org.json.JSONObject
 
 class RNSession(context: Context) : FrameLayout(context) {
 
@@ -16,12 +19,18 @@ class RNSession(context: Context) : FrameLayout(context) {
         val activity = reactContext.currentActivity as AppCompatActivity
         val webView = TurboWebView(context, null)
         val sessionName = UUID.randomUUID().toString()
-
+        webView.getSettings().setJavaScriptEnabled(true)
+        webView.addJavascriptInterface(JavaScriptInterface(), "AndroidInterface")
+        
         TurboSession(sessionName, activity, webView)
     }
 
     private val reactContext = context as ReactContext
     private val registeredVisitableViews = mutableListOf<SessionSubscriber>()
+
+    fun sendEvent(event: RNSessionEvent, params: WritableMap) {
+        reactContext.getJSModule(RCTEventEmitter::class.java).receiveEvent(id, event.name, params)
+    }
 
     internal fun registerVisitableView(newView: SessionSubscriber) {
         var callbacksCount = registeredVisitableViews.size
@@ -52,5 +61,15 @@ class RNSession(context: Context) : FrameLayout(context) {
 
     internal fun removeVisitableView(view: SessionSubscriber) {
         registeredVisitableViews.remove(view)
+    }
+
+    inner class JavaScriptInterface {
+        @JavascriptInterface
+        fun postMessage(messageStr: String) {
+            Log.d("RNVisitableView", "postMessage ${messageStr}")
+            // Android interface works only with primitive types, that's why we need to use JSON
+            val messageObj = Utils.convertJsonToBundle(JSONObject(messageStr)) // TODO remove double conversion
+            sendEvent(RNSessionEvent.RECEIVED_JS_MESSAGE, Arguments.fromBundle(messageObj))
+        }
     }
 }
--- android/app/src/main/java/com/hotwirerndemo/RNVisitableViewManager.kt
+++ android/app/src/main/java/com/hotwirerndemo/RNVisitableViewManager.kt
@@ -1,11 +1,9 @@
 package com.hotwirerndemo
 
-import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.UIManagerModule
-import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.annotations.ReactProp
 import com.th3rdwave.safeareacontext.getReactContext
 
