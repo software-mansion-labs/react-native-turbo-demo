a737303b440c5bec31a2a684cc181df5c22aac81
Mattias Pfeiffer <mattias@pfeiffer.dk>
2022-11-18 08:48:09 +0100

    Remove unused constants from config

--- example/src/config.ts
+++ example/src/config.ts
@@ -1,24 +1,12 @@
 export const BASE_URL = 'http://localhost:45678/';
 
 export enum Routes {
-  TabBar = 'TabBar',
   NotFound = 'NotFound',
   NumbersScreen = 'NumbersScreen',
-  PlaceholderScreen2 = 'PlaceholderScreen2',
-  PlaceholderScreen3 = 'PlaceholderScreen3',
   WebviewInitial = 'WebviewInitial',
   New = 'New',
-  One = 'One',
-  Two = 'Two',
-  Slow = 'Slow',
-  LongScreen = 'LongScreen',
   SuccessScreen = 'SuccessScreen',
   NonExistentScreen = 'NonExistentScreen',
   SignIn = 'SignIn',
-  Protected = 'Protected',
-  Files = 'Files',
-  Follow = 'Follow',
-  Redirected = 'Redirected',
-  Share = 'Share',
   Fallback = 'Fallback',
 }
