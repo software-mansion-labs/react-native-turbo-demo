7525db9495e64acda048dafab4287834e500b460
Patryk Klatka <patryk.klatka@gmail.com>
2023-11-16 13:32:23 +0100

    feat: support applicationNameForUserAgent on iOS

--- packages/turbo/ios/RNVisitableViewModule.m
+++ packages/turbo/ios/RNVisitableViewModule.m
@@ -10,6 +10,7 @@
 @interface RCT_EXTERN_MODULE(RNVisitableViewModule, NSObject)
 
   RCT_EXTERN_METHOD(setConfiguration: (nonnull NSString) sessionHandle
+                    applicationNameForUserAgent: (NSString) applicationNameForUserAgent
                     resolver: (RCTPromiseResolveBlock) resolve
                     rejecter: (RCTPromiseRejectBlock) reject)
 
