70ce7b1c2e38d3e2461bc4068578aae5c1d819c1
Miron Pawlik <miron.pawlik@swmansion.com>
2023-11-29 14:04:16 +0100

    feat: implement share screen in example code

--- example/src/ShareScreen.tsx
+++ example/src/ShareScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import type { SessionMessageCallback } from 'react-native-turbo';
 import WebView, { type Props } from './WebView';
+import { Share } from 'react-native';
+
+type MessageType = { message: { method: 'share'; shareText: string } };
 
 const ShareScreen: React.FC<Props> = (props) => {
-  const onMessage: SessionMessageCallback = (message) => {
-    console.log(message);
+  const onMessage: SessionMessageCallback = async (message) => {
+    const {
+      message: { shareText },
+    } = message as MessageType;
+    Share.share({
+      title: 'Share React Native Web Screen library',
+      message: shareText,
+    });
   };
   return <WebView {...props} onMessage={onMessage} />;
 };
