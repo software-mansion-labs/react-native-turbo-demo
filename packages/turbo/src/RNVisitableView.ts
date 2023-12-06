e3e4d0c9641ec9fbe0d6fe80a974b99e63acd568
Miron Pawlik <mironiasty@users.noreply.github.com>
2023-12-06 09:36:19 +0100

    Update RNVisitableView with better Typescript (#68)
    * feat: move RNVisitableView to new file
    
    * feat: add type to dispatch command
    
    * feat: add props to RNVisitableView
    
    * fix: check for optional
    
    * refactor: add comments to types

---
+++ packages/turbo/src/RNVisitableView.ts
@@ -0,0 +1,56 @@
+import {
+  NativeSyntheticEvent,
+  Platform,
+  requireNativeComponent,
+  StyleProp,
+  UIManager,
+  ViewStyle,
+} from 'react-native';
+import { findNodeHandle } from 'react-native';
+import {
+  DispatchCommandTypes,
+  OnLoadEvent,
+  VisitProposal,
+  VisitProposalError,
+} from './types';
+
+// interface should match RNVisitableView exported properties in native code
+interface RNVisitableViewProps {
+  url: string;
+  sessionHandle?: string;
+  applicationNameForUserAgent?: string;
+  onLoad?: (proposal: NativeSyntheticEvent<OnLoadEvent>) => void;
+  onMessage?: (proposal: NativeSyntheticEvent<{ message: object }>) => void;
+  onVisitError?: (e: NativeSyntheticEvent<VisitProposalError>) => void;
+  onVisitProposal?: (e: NativeSyntheticEvent<VisitProposal>) => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+const LINKING_ERROR =
+  `The package react-native-turbo doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+export function dispatchCommand(
+  ref: React.RefObject<any>,
+  command: DispatchCommandTypes,
+  code: string
+) {
+  const viewConfig = UIManager.getViewManagerConfig('RNVisitableView');
+
+  if (!viewConfig) {
+    throw new Error(LINKING_ERROR);
+  }
+
+  UIManager.dispatchViewManagerCommand(
+    findNodeHandle(ref.current),
+    viewConfig.Commands[command]!,
+    [code]
+  );
+}
+
+const RNVisitableView =
+  requireNativeComponent<RNVisitableViewProps>('RNVisitableView');
+
+export default RNVisitableView;
