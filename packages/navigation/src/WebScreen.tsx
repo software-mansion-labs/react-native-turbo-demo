7cbbad6ecc89283717ce023d8d6673533e56a8d4
Miron Pawlik <miron.pawlik@swmansion.com>
2023-12-06 16:18:47 +0100

    feat: add usage to useCurrentUrl

--- packages/navigation/src/WebScreen.tsx
+++ packages/navigation/src/WebScreen.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { LoadEvent, VisitableView, VisitProposal } from 'react-native-turbo';
 import { useWebviewNavigate } from './hooks/useWebviewNavigate';
+import { useCurrentUrl } from './hooks/useCurrentUrl';
 
 interface Props {
   navigation: any;
-  route: any;
   baseURL: string;
 }
 
 const WebScreen: React.FC<Props> = (props) => {
-  const { navigation, route } = props;
+  const { navigation } = props;
   const navigateTo = useWebviewNavigate();
-
-  const path = route?.params?.path || route?.path || '';
-  const baseURL = route?.params?.baseURL || props?.baseURL;
-  const currentUrl = `${baseURL}${path}`;
+  const currentUrl = useCurrentUrl();
 
   const onVisitProposal = ({ action: actionType, url }: VisitProposal) => {
     navigateTo(url, actionType);
--- packages/navigation/src/hooks/useCurrentUrl.ts
+++ packages/navigation/src/hooks/useCurrentUrl.ts
@@ -1,13 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
 
-export function useCurrentUrl(deafultBaseURL: string | undefined) {
+export function useCurrentUrl(defaultBaseUrl?: string) {
   const navigation = useNavigation();
   const state = navigation.getState();
+
   const currentRoute = state.routes[state.index];
   const params = currentRoute?.params as
     | { path?: string; baseURL?: string }
     | undefined;
+
   const path = params?.path ?? '';
-  const baseURL = params?.baseURL ?? deafultBaseURL ?? '';
+  const baseURL = params?.baseURL ?? defaultBaseUrl ?? '';
+
   return `${baseURL}${path}`;
 }
