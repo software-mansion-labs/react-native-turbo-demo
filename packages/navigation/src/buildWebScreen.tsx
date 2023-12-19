12522779c1d73e902c393738e03ac181d55e7f68
Miron Pawlik <miron.pawlik@swmansion.com>
2023-12-19 19:33:34 +0100

    fix: get proper params

--- packages/navigation/src/buildWebScreen.tsx
+++ packages/navigation/src/buildWebScreen.tsx
@@ -1,8 +1,22 @@
-import { LinkingOptions, getStateFromPath } from '@react-navigation/native';
+import {
+  LinkingOptions,
+  PartialRoute,
+  Route,
+  getStateFromPath,
+} from '@react-navigation/native';
 
 type Options = Parameters<typeof getStateFromPath>[1];
 
 type LinkedParams = { baseURL: string; fullPath: string };
+
+function getParams(
+  routes: PartialRoute<Route<string, object | undefined>>[] | undefined
+) {
+  if (routes?.[0]?.state?.routes) {
+    return getParams(routes[0].state.routes);
+  }
+  return routes?.[0]?.params as LinkedParams | undefined;
+}
 
 export function getLinkingObject(
   baseURL: string,
@@ -13,7 +27,7 @@ export function getLinkingObject(
     config: linking,
     getStateFromPath(path: string, options?: Options) {
       const state = getStateFromPath(path, options);
-      const params = state?.routes[0]?.params as LinkedParams | undefined;
+      const params = getParams(state?.routes);
       if (params) {
         params.baseURL = baseURL;
         params.fullPath = path;
