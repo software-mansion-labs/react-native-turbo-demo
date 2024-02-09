2269c131debd2c34c2797484c25f7c38468435dc
Patryk Klatka <patryk.klatka@gmail.com>
2024-02-09 11:22:27 +0100

    fix: example app

--- example/src/useSessionHandle.ts
+++ example/src/useSessionHandle.ts
@@ -16,7 +16,7 @@ function unpackState(state: NavigationState | PartialState<NavigationState>) {
 export function useSessionHandle() {
   const navigation = useNavigation();
   const state = unpackState(navigation.getState());
-  if (!state.routeNames || !state.index) {
+  if (!state.routeNames || (!state.index && state.index !== 0)) {
     return 'Default';
   }
   return state.routeNames[state.index];
