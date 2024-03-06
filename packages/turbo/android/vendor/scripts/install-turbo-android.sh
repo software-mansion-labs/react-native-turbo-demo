4a3db472607a860d97c54a80df6061130b109409
Patryk Klatka <patryk.klatka@gmail.com>
2024-03-06 16:40:18 +0100

    feat: auto get dependencies from turbo-android

--- packages/turbo/android/vendor/scripts/install-turbo-android.sh
+++ packages/turbo/android/vendor/scripts/install-turbo-android.sh
@@ -23,3 +23,23 @@ git apply ../patches/react-native-support.patch
 # Copy .aar file to lib directory
 LIB_DIR=$(realpath ../../libs)
 cp ./turbo/build/outputs/aar/turbo-release.aar $LIB_DIR/turbo-android.aar
+
+# Get necessary dependencies from turbo build.gradle
+./gradlew turbo:dependencies --configuration implementation > deps.txt
+./gradlew turbo:dependencies --configuration api > api.txt
+
+cd ../..
+DEPENDENCIES_GRADLE_FILE="dependencies.gradle"
+
+touch $DEPENDENCIES_GRADLE_FILE
+
+echo "ext {\n\tdeps = [  " > $DEPENDENCIES_GRADLE_FILE
+
+grep -oE  "[a-zA-Z0-9.\-]+:[a-zA-Z0-9.\-]+:[0-9a-zA-Z.\-]+" ./vendor/turbo-android/deps.txt | awk '{print "\t\tdep" NR ": " "\""$1"\"" ","}' >> $DEPENDENCIES_GRADLE_FILE
+
+echo "\t]\n\tapis = [" >> $DEPENDENCIES_GRADLE_FILE
+
+grep -oE  "[a-zA-Z0-9.\-]+:[a-zA-Z0-9.\-]+:[0-9a-zA-Z.\-]+" ./vendor/turbo-android/api.txt | awk '{print "\t\tapi" NR ": " "\""$1"\"" ","}' >> $DEPENDENCIES_GRADLE_FILE
+
+echo "\t] \n}" >> $DEPENDENCIES_GRADLE_FILE
+
