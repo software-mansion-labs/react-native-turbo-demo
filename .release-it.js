69de0b2dd9245755264922bb8382d3c708359d1b
Patryk Klatka <patryk.klatka@gmail.com>
2024-03-19 15:59:26 +0100

    fix: push changes to separate branch

--- .release-it.js
+++ .release-it.js
@@ -11,13 +11,17 @@ module.exports = {
     push: true,
     requireCommits: true,
     requireCommitsFail: false,
+    requireUpstream: false,
     commitMessage: 'Release ${name}@${version}',
+    pushArgs: ['--follow-tags', '--force'],
   },
   github: {
     release: true,
     releaseName: '${name}@${version}',
   },
   hooks: {
-    'before:init': 'turbo build',
+    'before:init': 'git checkout -B next && turbo build',
+    'after:release':
+      'gh pr create --body "Release new version of the library." -w && git checkout main',
   },
 };
