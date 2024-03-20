2758e28d6f820c95a7329a2bf391f9d1ddf324fb
Mattias Pfeiffer <mattias@pfeiffer.dk>
2024-03-20 12:56:17 +0100

    Fix issue with routes in `getStateForPath` (#158)
    Co-authored-by: Patryk Klatka <patryk.klatka@gmail.com>

--- packages/navigation/src/buildWebScreen.tsx
+++ packages/navigation/src/buildWebScreen.tsx
@@ -1,28 +1,25 @@
-import {
-  PartialRoute,
-  Route,
-  getStateFromPath,
-} from '@react-navigation/native';
+import type { NavigationState } from '@react-navigation/core';
+import { getStateFromPath, PartialState } from '@react-navigation/native';
 
 import { LinkingConfig } from './hooks/useCurrentUrl';
 import { unpackState } from './utils/unpackState';
 
 type Options = Parameters<typeof getStateFromPath>[1];
 type LinkedParams = { baseURL?: string; fullPath?: string };
-type PartialRoutes = readonly PartialRoute<Route<string, object | undefined>>[];
 
 function getParams(
-  routes: PartialRoutes | undefined
+  state: NavigationState | PartialState<NavigationState>
 ): LinkedParams | undefined {
-  const firstRoute = routes?.[0];
-  if (firstRoute) {
-    if (firstRoute.params) {
-      return firstRoute.params;
+  const activeRoute = state.routes?.[state.index ?? 0];
+
+  if (activeRoute) {
+    if (activeRoute.params) {
+      return activeRoute.params;
     } else {
       // route is readonly object (in types). But we need to hack it be non-empty
       // @ts-expect-error
-      firstRoute.params = {};
-      return firstRoute.params;
+      activeRoute.params = {};
+      return activeRoute.params;
     }
   }
   return undefined;
@@ -35,7 +32,7 @@ export function getLinkingObject(baseURL: string, linking: LinkingConfig) {
     getStateFromPath(path: string, options?: Options) {
       const state = getStateFromPath(path, options);
       if (state) {
-        const params = getParams(unpackState(state)?.routes);
+        const params = getParams(unpackState(state));
         if (params) {
           params.baseURL = baseURL;
           params.fullPath = path;
