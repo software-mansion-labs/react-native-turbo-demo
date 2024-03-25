e02328969feec042dfd55dcda8cba757895cc369
Patryk Klatka <patryk.klatka@gmail.com>
2024-03-25 18:05:08 +0100

    feat: modify releases

---
+++ scripts/release.sh
@@ -0,0 +1,29 @@
+PACKAGE_NAME=$(grep -m1 name package.json | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+OLD_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+# Create a new branch
+git checkout -B next
+
+# Build the library
+turbo build
+
+# Run release-it
+release-it --config ../../.release-it.js --only-version
+
+git checkout next
+
+VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+echo "New version: $VERSION"
+
+# Create release PR and merge it
+gh pr create --title "Release $PACKAGE_NAME@$VERSION" --body "Release new version of the library." 
+PR_NUMBER=$(gh pr list | grep "$PACKAGE_NAME@$VERSION" | awk '{print $1}')
+gh pr merge $PR_NUMBER --squash --delete-branch
+
+# Create release
+gh release create "$PACKAGE_NAME@$VERSION" --title "$PACKAGE_NAME@$VERSION" --generate-notes --notes-start-tag "$PACKAGE_NAME@$OLD_VERSION"
+
+# Cleanup 
+git checkout .
+git checkout main
