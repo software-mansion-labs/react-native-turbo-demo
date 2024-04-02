5afc3b34df317a7b1dc6d81829551d1f7ded8784
Patryk Klatka <patryk.klatka@gmail.com>
2024-04-02 11:40:50 +0200

    feat: early return

--- packages/turbo/scripts/postinstall.sh
+++ packages/turbo/scripts/postinstall.sh
@@ -1,11 +1,12 @@
-VENDOR_DIR=$(realpath ./android/vendor)
-PARENT_FOLDER=$(realpath .. | awk -F/ '{print $NF}')
-
-if [ ! -d "$VENDOR_DIR" ]; then
+# Check if android/vendor directory exists
+if [ ! -d "./android/vendor" ]; then
   # If the vendor folder does not exist, we're not using the published package.
   # We can safely exit here, because postinstall script will be executed again when vendor folder is created. 
   exit 0
 fi
+
+VENDOR_DIR=$(realpath ./android/vendor)
+PARENT_FOLDER=$(realpath .. | awk -F/ '{print $NF}')
 
 if [ "$PARENT_FOLDER" = "node_modules" ]; then
   REACT_NATIVE_DIR=$(realpath ../react-native/android)
