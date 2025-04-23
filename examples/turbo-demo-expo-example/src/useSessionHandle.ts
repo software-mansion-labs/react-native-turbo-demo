73408da3545d4f4de2de7794a220abc9ecf77bf7
Bartosz Szewczyk <bartosz.szewczyk@swmansion.com>
2025-04-23 18:35:02 +0200

    chore: fix typescript task for examples

--- examples/turbo-demo-expo-example/src/useSessionHandle.ts
+++ examples/turbo-demo-expo-example/src/useSessionHandle.ts
@@ -4,19 +4,21 @@ import {
   useNavigation,
 } from '@react-navigation/native';
 
-function unpackState(state: NavigationState | PartialState<NavigationState>) {
+function unpackState(
+  state: NavigationState | PartialState<NavigationState> | undefined
+) {
   const nestedState = state?.routes[0]?.state;
   if (nestedState) {
     return unpackState(nestedState);
-  } else {
-    return state;
   }
+
+  return state;
 }
 
 export function useSessionHandle() {
   const navigation = useNavigation();
   const state = unpackState(navigation.getState());
-  if (!state.routeNames || (!state.index && state.index !== 0)) {
+  if (!state?.routeNames || (!state.index && state.index !== 0)) {
     return 'Default';
   }
   return state.routeNames[state.index];
